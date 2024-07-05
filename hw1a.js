59bff18dc95989283c7a9534eaeb38b236b2f09c
“lest”-xu <autodataxx@gmail.com>
2024-07-05 12:54:09 -0700

    update

--- hw1a.js
+++ hw1a.js
@@ -1,5 +1,34 @@
 const cities = require('./zipCodeModule_v1');
 const colors = require('colors');
 
+// Lookup by zip code: 02215, and print the results.
+cities.lookupByZipCode('02215');
+console.log(); // add a new line
 
+// Lookup by zip code: 99999, and print the results.
+cities.lookupByZipCode('99999');
+console.log(); // add a new line
 
+// Lookup by city, state: BOSTON, MA, and print the results.
+cities.lookupByCityState('BOSTON', 'MA');
+console.log(); // add a new line
+
+// Lookup by city, state: BOSTON, TX, and print the results.
+cities.lookupByCityState('BOSTON', 'TX');
+console.log(); // add a new line
+
+// Lookup by city, state: BOSTON, AK, and print the results.
+cities.lookupByCityState('BOSTON', 'AK');
+console.log(); // add a new line
+
+// Get population by state: MA, and print the results.
+cities.getPopulationByState('MA');
+console.log(); // add a new line
+
+// Get population by state: TX, and print the results.
+cities.getPopulationByState('TX');
+console.log(); // add a new line
+
+// Get population by state: AA, and print the results
+cities.getPopulationByState('AA');
+console.log(); // add a new line
--- zipCodeModule_v1.js
+++ zipCodeModule_v1.js
@@ -1,14 +1,48 @@
 const data = require('./zips.json');
 
-module.exports.lookupByZipCode =  (zip) => {
-		
+module.exports.lookupByZipCode = (zip) => {
+    // make sure zip is provided
+    if (zip) {
+        // print info
+        console.log(`Look up by zip code (${zip})`);
+        // loop through the data
+        for (let i = 0; i < data.length; i++) {
+            // create result obejct
+            const item = data[i];
+            if (item._id === zip) {
+                // found, print the result and return
+                console.log(item);
+                return item;
+            }
+        }
+    }
+    // not found, print and return undefined
+    console.log(undefined);
+    return undefined;
 };
 
 module.exports.lookupByCityState = (city, state) => {
-		
+    // print info
+    console.log(`Look up by city (${city}, ${state})`);
+    // create the result obejct for return
+    let result = {
+        'city': city,
+        'state': state,
+        'data': []
+    }
+    for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        if (item.city === city && item.state === state) {
+            // found, add to city data
+            result.data.push({'city': item.city, 'state': item.state});
+        }
+    }
+    // print the result and return
+    console.log(result);
+    return result;
 };
 
 module.exports.getPopulationByState = (state) => {
-		
+
 };
 
